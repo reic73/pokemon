71a554c956b7275427cb743b334149d379a60e50
rico <rico@fivejack.com>
2022-01-26 15:01:10 +0700

    setup tailwind and material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Provider} from 'react-redux'
+import "tailwindcss/tailwind.css"
 
 import store from '../redux/store'
 
