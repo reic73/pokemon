09bfe9089584303d2dc5354ed6d59e12357fad27
rico <rico@fivejack.com>
2022-01-26 14:30:18 +0700

    setup initial page retrieve pokemon list data remove code from main index

--- api/request.ts
+++ api/request.ts
@@ -2,12 +2,23 @@ import Axios from "axios"
 
 export default class Request{
   public static async retrievePokemonLists (){
-    try{
-      const toReturn = await Axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    try{ 
+      const response = await Axios.get('https://pokeapi.co/api/v2/pokemon?limit=2')
+      
+      const responseData = response.data
+      const pokemonData = responseData.results
+      const promises = pokemonData.map(async(data:any)=>{
+        const pokemonDetails = await Axios.get(data.url)
+        data['id'] = pokemonDetails.data.id
+        data['image'] = pokemonDetails.data.sprites.front_default
+      })
+      await Promise.all(promises)
+
+      const toReturn = pokemonData
       return toReturn
     }catch(e){
      alert('Terjadi kesalahan mengambil data list Pokemon')
-      return {status:404}
+      return {}
     }
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pokemon-list',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
